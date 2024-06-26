4fb9a50e6c444514b55c7dd3cc744aaf684872bc
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-06-26 13:34:47 +0530

    Update AirDropPage.jsx

--- src/AirDropPage.jsx
+++ src/AirDropPage.jsx
@@ -79,7 +79,7 @@ const AirdropPage = () => {
         <p className="text-sm mb-2 text-white">Received: 6,000,000</p>
         <p className="text-sm mb-6 text-white">Goal: 10,000,000</p>
         <button className="claim-button bg-gradient-to-r from-pink-500 to-purple-500 text-white px-8 py-4 rounded-full text-lg font-bold shadow-lg"  data-testid="connect-button">
-          <w3m-connect-button>Claim Now</w3m-connect-button>
+          <w3m-account-button>Claim Now</w3m-account-button>
         </button>
       </div>
       <p className="absolute bottom-1 text-center w-full text-black">Stash-airdrop © 2024</p>
